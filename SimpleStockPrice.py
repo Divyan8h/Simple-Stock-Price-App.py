ac31947f18968d4e655d933c9705c98400ce356e
Divyan8h <99311459+Divyan8h@users.noreply.github.com>
2023-12-20 12:50:00 +0530

    Add files via upload

---
+++ SimpleStockPrice.py
@@ -0,0 +1,22 @@
+import yfinance as yf #used for analysing financial data
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock **closing price** and ***volume*** of Apple!
+
+"""
+	)
+
+tickersymbol='AAPL'
+
+tickerdata = yf.Ticker(tickersymbol) #Ticker used for interacting with Yahoo Finance API
+
+tickerDf = tickerdata.history(period='1d', start='2022-5-31', end='2023-5-31')
+
+#st.write(tickerDf.columns)
+
+st.line_chart(tickerDf['Close'])
+st.line_chart(tickerDf['Volume'])
